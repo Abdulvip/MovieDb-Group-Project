d82ca1c37ac9d469a9d71b83fac58bb6ef5eb84c
rachel <rachelohler@gmail.com>
2020-08-21 16:51:03 -0400

    updated service

--- src/app/movie-db.service.ts
+++ src/app/movie-db.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieDbService {
+  baseUrl: string = 'https://api.themoviedb.org/3/movie/550?';
+  key: string = '24762c0485091b5bfc7fcf403a30da23';
 
-  constructor() { }
+  constructor() {}
 }
