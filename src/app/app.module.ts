71585a42913b23930fb3be8744351badd2bf8a13
rachel <rachelohler@gmail.com>
2020-08-21 16:28:38 -0400

    framework made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { WatchlistPageComponent } from './watchlist-page/watchlist-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchCriteriaComponent,
+    MovieListComponent,
+    WatchlistPageComponent
   ],
   imports: [
     BrowserModule,
